2e8ccc1bd1e6d34a0cd736fb01d02f39aead648c
YunWang <wangyqwe@qq.com>
2021-03-30 19:32:22 +0800

    原有电商项目改造成app介绍页面

--- js/index-vue.js
+++ js/index-vue.js
@@ -3,13 +3,14 @@
 		el:'#index',
 		data:{
 			products:[
-				{src:'image/product/item1.jpg',title:'民族服饰_纯手工绣花青蓝色',price:220,author:'小飞象',id:1},
-				{src:'image/product/item2.jpg',title:'做个东西_月球灯挂壁灯家居装饰纯手工',price:15,author:'做个东西',id:2},
-				{src:'image/product/item3.jpg',title:'做个东西_流光灯床头灯家居装饰纯手工',price:166,author:'做个东西',id:3},
-				{src:'image/product/item4.jpg',title:'入云屋_水洗帆布袋秋叶系列原创',price:125,author:'入云屋',id:4},{src:'image/product/item1.jpg',title:'民族服饰_纯手工绣花青蓝色',price:220,author:'小飞象',id:5},
-				{src:'image/product/item2.jpg',title:'做个东西_月球灯挂壁灯家居装饰纯手工',price:15,author:'做个东西',id:6},
-				{src:'image/product/item3.jpg',title:'做个东西_流光灯床头灯家居装饰纯手工',price:166,author:'做个东西',id:7},
-				{src:'image/product/item4.jpg',title:'入云屋_水洗帆布袋秋叶系列原创',price:125,author:'入云屋',id:8}
+				{src:'image/0.jpg'},
+				{src:'image/1.jpg'},
+				{src:'image/2.jpg'},
+				{src:'image/3.jpg'},
+				{src:'image/4.jpg'},
+				{src:'image/5.jpg'},
+				{src:'image/6.jpg'},
+				{src:'image/7.jpg'}
 			],
 			user:{"username":"","handsPassword":"","fakename":"汉织生活","sex":true,"birthday":"","headimg":"image/icon/头像.png","money":0,"phone":""}
 		},
